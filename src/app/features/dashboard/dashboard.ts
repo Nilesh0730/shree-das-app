06751a40e1fedd455debb52fb1c994197aaa867b
prathamesh <pratham10041998@gmail.com>
2026-02-14 15:20:02 +0530

    dashboard styling

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  userMarster() {
+        alert("userMarster was clicked!");
+    }
+    businessMaster() {
+        alert("businessMaster was clicked!");
+    }
+    sadasyaMahiti() {
+        alert("sadasyaMahiti was clicked!");
+    }
+    logout() {
+        alert("logout was clicked!");
+    }
 }
